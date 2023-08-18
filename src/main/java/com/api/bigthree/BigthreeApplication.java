f43ab1acdfdbfb5ba212362b1526fce6b2644746
Paulo Cesar Missagia Filho <missagiafilhop@emea.cshare.net>
2023-08-18 15:36:31 +0100

    intial work

---
+++ src/main/java/com/api/bigthree/BigthreeApplication.java
@@ -0,0 +1,13 @@
+package com.api.bigthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigthreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigthreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/bigthree/BigthreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.bigthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigthreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
