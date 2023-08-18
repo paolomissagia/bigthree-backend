0c5e655c12ca5aa82e57207a6429578fa4c55ce1
Paulo Cesar Missagia Filho <missagiafilhop@emea.cshare.net>
2023-08-18 15:36:58 +0100

    created model for Customer

---
+++ src/main/java/com/api/bigthree/customer/Customer.java
@@ -0,0 +1,13 @@
+package com.api.bigthree.customer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Customer {
+    private Long customerId;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
